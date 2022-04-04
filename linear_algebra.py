224783ae33a7ceada00670e4d44dd22d9bd5ee98
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-04-04 03:06:45 +0000

    Update linear_algebra.py

--- linear_algebra.py
+++ linear_algebra.py
@@ -42,4 +42,4 @@ def scalar_multiply(c: float , v:Vector) -> Vector:
     """Multiplies every element by c"""
     return [c * v_i for v_i in v]
 
-assert scalar_multiply(2,[1,2,3]) == [2,4,6]
+assert scalar_multiply(2,[1,2,3]) == [2,4,6],'ayoub'
