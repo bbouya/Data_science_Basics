3d83fd2285308340c27a21d6f07d0c5daad7354a
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-04-04 02:55:45 +0000

    Update linear_algebra.py

--- linear_algebra.py
+++ linear_algebra.py
@@ -26,4 +26,13 @@ def subtract(v: Vector, w: Vector) -> Vector:
 
 assert subtract([5,7,9],[4,5,6]) == [1,2,3]
 
-def vector_sum(vectors: List[Vectors])
+def vector_sum(vectors: List[Vector])-> Vector:
+    """Sums all corresponding elements"""
+    # CHeck that vectors is not empty
+    assert vectors, "no verctors provided"
+    # CHeck the vectors are all the same size
+    num_elements = len(vectors[0])
+    assert all(len(v) == num_elements for v in vectors), "different sizes."
+    # the i-th element of the result is the sum of every vector[i]
+    return [sum(vector[i] for vector in vectors) for i in range(num_elements)]
+
