540927a93dcc4f105e0606e86220c598041e5eae
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-04-04 12:09:10 +0000

    clustering

--- clustering.py
+++ clustering.py
@@ -1,1 +1,8 @@
+from turtle import Vec2D
 from linear_algebra import Vector
+
+
+def num_differences(v1: Vector, v2: Vector) -> int:
+    assert len(v1) == len(v2)
+    return len([x1 for x1,x1 in zip(v1,v2) if x1!=x2])
+
