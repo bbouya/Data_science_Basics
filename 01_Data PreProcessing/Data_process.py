7fc392c3f011e44976b5279da2b7006d126278a9
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-04-13 20:38:57 +0000

    Create Data_process.py

---
+++ 01_Data PreProcessing/Data_process.py
@@ -0,0 +1,28 @@
+#importing libraries 
+
+import imp
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing dataset 
+dataset = pd.read_csv('Data.csv')
+
+#Seperating independent variable from dependent
+
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+# Handling the missing data
+# take care of missing data in dataset
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+
+# To select columns from where we have to calculate data
+imputer = imputer.fit(X[:,1:3])
+# To make changes to the original dataset 
+
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+# To deal with categorical data we had to convert it into numbers
